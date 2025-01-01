0d7f534b6a569f675e4fb7701d497e530cd8eb10
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2025-01-02 00:56:42 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from pyrogram import Client
+from pyrogram.storage import MemorySession
 
 # Replace with your actual session string
 session_string = "BQF0lboAcW3xr4pSLlwcg35NnGhsrXIFNoMVEyftP9M5m5mrN_WhmhhqVNWS2MiTBUsj34EykN_ENQ4Kgm8IuFVMRCae-6mlpM1m3zYLPd6Pn6gaet-aNgEtbedXrjOLRSMXCcwn6_ujVVIi2GBn9nQDNj_J6c527rvmIJ6LYra8MOoBk90T6E_me95ORsOfeyKaDyY_GMzK_Q7VRRv2IUwmH9HAm6SG4FBUerOpB5oVoVEqKXYAT8WbGZs3FsfP2Hr91mqQNs4tcv-6jfKPkuA9t4etrqC-Rqe6Awm5ezOPYIjy-r-CrkvpFTLRe_vsT2h12abCs826-bX3eV09hsI4UjNJjwAAAAF9OlgPAA"
@@ -7,7 +8,8 @@ session_string = "BQF0lboAcW3xr4pSLlwcg35NnGhsrXIFNoMVEyftP9M5m5mrN_WhmhhqVNWS2M
 api_id = 24417722  # Replace with your API ID
 api_hash = "68c75f726da6bd11acda8d7cd03e89f3"  # Replace with your API Hash
 
-app = Client(session_string, api_id=api_id, api_hash=api_hash)
+# Use MemorySession for temporary session storage
+app = Client("my_bot", session_string=session_string, api_id=api_id, api_hash=api_hash, storage=MemorySession())
 
 with app:
     me = app.get_me()
