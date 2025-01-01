85a472b1aaedb48e729634563b3a2a2949cb340d
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2025-01-02 00:53:29 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from pyrogram import Client
-from pyrogram.storage import MemoryStorage
 
 # Replace 'your_string_session' with your session string
 session_string = "BQF0lboAcW3xr4pSLlwcg35NnGhsrXIFNoMVEyftP9M5m5mrN_WhmhhqVNWS2MiTBUsj34EykN_ENQ4Kgm8IuFVMRCae-6mlpM1m3zYLPd6Pn6gaet-aNgEtbedXrjOLRSMXCcwn6_ujVVIi2GBn9nQDNj_J6c527rvmIJ6LYra8MOoBk90T6E_me95ORsOfeyKaDyY_GMzK_Q7VRRv2IUwmH9HAm6SG4FBUerOpB5oVoVEqKXYAT8WbGZs3FsfP2Hr91mqQNs4tcv-6jfKPkuA9t4etrqC-Rqe6Awm5ezOPYIjy-r-CrkvpFTLRe_vsT2h12abCs826-bX3eV09hsI4UjNJjwAAAAF9OlgPAA"
@@ -8,8 +7,8 @@ session_string = "BQF0lboAcW3xr4pSLlwcg35NnGhsrXIFNoMVEyftP9M5m5mrN_WhmhhqVNWS2M
 api_id = 24417722  # Replace with your API ID
 api_hash = "68c75f726da6bd11acda8d7cd03e89f3"  # Replace with your API Hash
 
-# Provide a name for the MemoryStorage
-app = Client(session_string, api_id=api_id, api_hash=api_hash, storage=MemoryStorage(name="my_session"))
+# Create the app instance without passing `storage` argument
+app = Client(session_string, api_id=api_id, api_hash=api_hash)
 
 with app:
     me = app.get_me()
